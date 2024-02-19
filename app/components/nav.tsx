a7d8175e9385679339b535145fb4152634b853c1
Donggi Lee <donggu5235@gmail.com>
2024-02-19 17:30:49 +0900

    240219

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React, { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 
 export const Nav: React.FC = () => {
